f78bbb28661ce5f81f83eca89e01118ae7f44993
Luis Gonzalez <intellto24@gmail.com>
2018-04-29 23:25:32 +0100

    added uri info

--- Main.rb
+++ Main.rb
@@ -6,7 +6,9 @@ require 'sidekiq/api'
 require 'sidekiq/web'
 
 
-
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
 	
 
 class OurWorker
