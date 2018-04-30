ffdb78d89e0643dd1d50e6bf9e2a1a9508f1ac7d
Luis Gonzalez <intellto24@gmail.com>
2018-04-30 22:43:19 +0100

    disable sidekiq for the day commented out code

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
-require 'sidekiq'
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:c6335ef7dd50390873ab720ce80bab44@soldierfish.redistogo.com:11528/' }
-end
+# require 'sidekiq'
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://redistogo:c6335ef7dd50390873ab720ce80bab44@soldierfish.redistogo.com:11528/' }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:c6335ef7dd50390873ab720ce80bab44@soldierfish.redistogo.com:11528/' }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://redistogo:c6335ef7dd50390873ab720ce80bab44@soldierfish.redistogo.com:11528/' }
+# end
