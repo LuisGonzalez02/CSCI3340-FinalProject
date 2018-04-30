1e68d738ac260807193868b1b42dbb7516fe745b
Luis Gonzalez <intellto24@gmail.com>
2018-04-30 22:20:15 +0100

    added sidekiq file

--- Worker.rb
+++ Worker.rb
@@ -2,13 +2,7 @@ require 'sidekiq'
 require 'sidekiq/api'
 require 'sidekiq/web'
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:c6335ef7dd50390873ab720ce80bab44@soldierfish.redistogo.com:11528/' }
-end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:c6335ef7dd50390873ab720ce80bab44@soldierfish.redistogo.com:11528/' }
-end
 
 class OurWorker
 	include Sidekiq::Worker
