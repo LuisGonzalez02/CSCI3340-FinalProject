d160ebdcf8cd7839c18d2f942b63a7e57b64ecc7
Luis Gonzalez <intellto24@gmail.com>
2018-04-30 02:21:05 +0100

    removed sidekiq file

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-require 'sidekiq'
-Sidekiq.configure_client do |config|
-	config.redis={url: ENV['REDIS_PROVIDER']}
-end
-Sidekiq.configure_server do |config|
-	config.redis={url: ENV['REDIS_PROVIDER']}
-end
