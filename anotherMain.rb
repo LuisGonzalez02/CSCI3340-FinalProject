3c7eac9e34c67fd420da5ce1f2e47f44f5ba7244
gabrielJay <36487502+gabrielJay@users.noreply.github.com>
2018-05-01 14:29:11 -0500

    Add files via upload
    I made a copy of the main file name anotherMain.rb, and made a few changes to run with the page2.erb file. now it covers the process of choosing a dialog, number, and time. then pops the stripe payment form, and finally redirects to charge.erb making the call in between the transition after the payment.

---
+++ anotherMain.rb
@@ -0,0 +1,83 @@
+require 'sinatra'
+require 'twilio-ruby'
+require 'sinatra/base'
+require 'uri'
+require './Worker.rb'
+require 'stripe'
+
+set :publishable_key, 'pk_test_Bt6PgAyfsT00OIRzjPVd3mQs'
+set :secret_key, 'sk_test_jExBMJHRY8EdED1PU0pALhqI'
+Stripe.api_key = settings.secret_key
+
+get '/' do
+	@chosenTime=params["exit-time"]
+	if(@chosenTime==nil)
+		@currentTime=getCurrentTime().to_s
+
+	else
+		@currentTime=@chosenTime.to_s
+
+	end
+
+	erb :page2
+end
+
+post '/testing' do
+	delayTime()
+end
+
+get '/stripe' do
+  erb :index
+end
+
+post '/charge' do
+	 # Amount in cents
+  @amount = 100
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge',
+    :currency    => 'usd',
+    :customer    => customer.id
+  )
+delayTime()
+  erb :charge
+end
+
+
+#function that returns the current time 
+def getCurrentTime()
+	return Time.now.strftime("%H:%M")
+end
+
+#function that does something after 10 seconds
+def delayTime()
+	account_sid = 'AC3040bfd66e0c15eeae03e6b7bd55443c'
+	auth_token = 'a10018bf4cfb66aa234e84b45960c3f5'
+
+	# set up a client to talk to the Twilio REST API
+	@client = Twilio::REST::Client.new account_sid, auth_token
+
+	def ffcaller(calledNumber)
+	call = @client.calls.create(
+	   to: calledNumber,
+	   from: "+12019928984",
+	   url: "https://handler.twilio.com/twiml/EHe5881b22b8f52a84a6b22a72c2882d39")
+	puts call.to
+	end
+
+	@newnumber=params[:number]
+	string= "+1#{@newnumber}"
+	ffcaller(string)
+end
+
+
+
+
+
+
