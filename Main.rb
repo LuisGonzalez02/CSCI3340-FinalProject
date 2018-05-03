047a2218c8a868692d8efa46f5ed50c301427997
Luis Gonzalez <intellto24@gmail.com>
2018-05-03 16:19:12 +0100

    with sidekiq v2

--- Main.rb
+++ Main.rb
@@ -11,25 +11,22 @@ require_relative 'lib/workers/test_worker.rb'
 
 
 class App < Sinatra::Base
+	set :publishable_key, 'pk_test_Bt6PgAyfsT00OIRzjPVd3mQs'
+	set :secret_key, 'sk_test_jExBMJHRY8EdED1PU0pALhqI'
+	Stripe.api_key = settings.secret_key
+
 
 	get '/' do
-		stats = Sidekiq::Stats.new
-		workers = Sidekiq::Workers.new
-		"
-		<p>Processed: #{stats.processed}</p>
-		<p>In Progress: #{workers.size}</p>
-		<p>Enqueued: #{stats.enqueued}</p>
-		<p><a href='/'>Refresh</a></p>
-		<p><a href='/add_job'>Add Job</a></p>
-		<p><a href='/sidekiq'>Dashboard</a></p>
-		"
+	@chosenTime=params["exit-time"]
+	if(@chosenTime==nil)
+		@currentTime=getCurrentTime().to_s
+
+	else
+		@currentTime=@chosenTime.to_s
+
 	end
 
-	get '/add_job' do
-		"
-		<p>Added Job: #{::TestWorker.perform_async(20)}</p>
-		<p><a href='/'>Back</a></p>
-		"
+	erb :mainPage
 	end
 	get '/getinfo' do
 
@@ -38,13 +35,6 @@ class App < Sinatra::Base
 
 	end
 
-	get '/testing' do
-		@please="Trying to fix"
-		#OurWorker.perform_in(10,"testing")
-		
-
-		return @please
-	end
 
 	get '/stripe' do
 	  erb :index
@@ -89,7 +79,24 @@ class App < Sinatra::Base
 		return Time.now.strftime("%H:%M")
 	end
 
+	#function that does something after 10 seconds
+	def delayTime()
+		account_sid = 'AC3040bfd66e0c15eeae03e6b7bd55443c'
+		auth_token = 'a10018bf4cfb66aa234e84b45960c3f5'
 
+		# set up a client to talk to the Twilio REST API
+		@client = Twilio::REST::Client.new account_sid, auth_token
+		def ffcaller(calledNumber)
+		call = @client.calls.create(
+		   to: calledNumber,
+		   from: "+12019928984",
+		   url: "https://handler.twilio.com/twiml/EHe5881b22b8f52a84a6b22a72c2882d39")
+		puts call.to
+		end
+		@newnumber=params[:number]
+		string= "+1#{@newnumber}"
+		ffcaller(string)
+	end
 
 
 
